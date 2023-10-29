e616107f351213265c818cbee69f9c5f463a48a0
ELRODRY <rodrigoezequielolivaresvcp@gmail.com>
2023-10-29 10:20:31 -0300

    first commit

---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
+
+export const GifGrid = ({ category }) => {
+
+    const { images, isLoading } = useFetchGifs(category);
+    console.log({ images, isLoading })
+
+
+    return (
+        <>
+            <h3>{category}</h3>
+            {
+                isLoading && (<h2>Carganndo...</h2>)
+            }
+            <div className="card-grid">
+                {images.map((image) => (
+                    <GifItem
+                        key={image.id}
+                        {...image}
+                    />
+                ))}
+
+            </div>
+        </>
+    );
+};
